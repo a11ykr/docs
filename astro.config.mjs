0d794a0db13c6d5e4b5fe83a50d49636c574bb1f
resistan <resistan@gmail.com>
2025-03-25 12:00:14 +0900

    glossary setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
+import astroExpressiveCode from 'astro-expressive-code';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://a11ykr.github.io/',
 	base: '/docs/',
 	integrations: [
+		astroExpressiveCode(),
+		mdx(),
 		starlight({
 			title: 'A11Y KR Docs',
 			logo: {
 				src: '/src/assets/a11ykr.png',
 				replacesTitle: true,
 			},
-			defaultLocale: 'root', // optional
+			defaultLocale: 'root',
 			locales: {
 				root: {
 					label: '한국어',
@@ -24,22 +26,14 @@ export default defineConfig({
 				github: 'https://a11ykr.github.io',
 			},
 			sidebar: [
-				// {
-				// 	label: '표준 관련 문서 모음',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: '목록', link: '/guides/' },
-				// 		{ label: '웹 콘텐츠 접근성 지침(WCAG) 2.2', link: 'https://a11ykr.github.io/wcag22/' },
-				// 	],
-				// },
 				{ label: '표준 관련 문서 모음', link: '/guides/' },
 				{ label: '참고 자료', link: '/reference/' },
 				{ label: '접근성 점검 도구', link: '/acts/' },
+				{
+					label: '용어집',
+					autogenerate: { directory: 'glossary' }
+				},
 			],
-			components: {
-			// Override the default `SocialIcons` component.
-			// SocialIcons: './src/components/EmailLink.astro',
-			}
-		},
-	)],
+		}),
+	],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,27 @@
 import { defineCollection, z } from 'astro:content';
 import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+// import { glob } from 'astro/loaders';
 
-export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-	i18n: defineCollection({
-    loader: i18nLoader(),
-    schema: i18nSchema({
-      extend: z.object({
-        'custom.label': z.string().optional(),
-      }),
+const docs = defineCollection({ loader: docsLoader(), schema: docsSchema() });
+const i18n = defineCollection({
+  loader: i18nLoader(),
+  schema: i18nSchema({
+    extend: z.object({
+      'custom.label': z.string().optional(),
     }),
   }),
-};
+});
+// 용어집 컬렉션 자동 구성
+const glossary = defineCollection({
+  loader: docsLoader(),
+  schema: docsSchema({
+    extend: z.object({
+      title: z.string(),
+      description: z.string().optional(),
+    }),
+  }),
+});
+
+
+export const collections = { docs, i18n, glossary };
